ccdf5362247ee4d3f8f0b2bd9ca743766ffe1802
Paulin Todev <paulin.todev@gmail.com>
2025-04-16 11:12:04 +0100

    Use Alloy user

--- main.rb
+++ main.rb
@@ -59,23 +59,23 @@ module Homebrew
   end
 
   # Get user details
-  user = GitHub::API.open_rest "#{GitHub::API_URL}/user"
-  user_id = user['id']
-  user_login = user['login']
-  user_name = user['name'] || user['login'] if user_name.blank?
-  user_email = user['email'] || (
-    # https://help.github.com/en/github/setting-up-and-managing-your-github-user-account/setting-your-commit-email-address
-    user_created_at = Date.parse user['created_at']
-    plus_after_date = Date.parse '2017-07-18'
-    need_plus_email = (user_created_at - plus_after_date).positive?
-    user_email = "#{user_login}@users.noreply.github.com"
-    user_email = "#{user_id}+#{user_email}" if need_plus_email
-    user_email
-  ) if user_email.blank?
+  # user = GitHub::API.open_rest "#{GitHub::API_URL}/user"
+  # user_id = user['id']
+  # user_login = user['login']
+  # user_name = user['name'] || user['login'] if user_name.blank?
+  # user_email = user['email'] || (
+  #   # https://help.github.com/en/github/setting-up-and-managing-your-github-user-account/setting-your-commit-email-address
+  #   user_created_at = Date.parse user['created_at']
+  #   plus_after_date = Date.parse '2017-07-18'
+  #   need_plus_email = (user_created_at - plus_after_date).positive?
+  #   user_email = "#{user_login}@users.noreply.github.com"
+  #   user_email = "#{user_id}+#{user_email}" if need_plus_email
+  #   user_email
+  # ) if user_email.blank?
 
   # Tell git who you are
-  git 'config', '--global', 'user.name', user_name
-  git 'config', '--global', 'user.email', user_email
+  git 'config', '--global', 'user.name', "grafana-alloybot[bot]"
+  git 'config', '--global', 'user.email', "879451+grafana-alloybot[bot]@users.noreply.github.com"
 
   if tap.blank?
     brew 'tap', 'homebrew/core', '--force'
